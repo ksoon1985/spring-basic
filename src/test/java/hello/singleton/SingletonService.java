095479898661dd0c3dcc18905f3703492e65707b
ksoon1985 <ksoon1985@naver.com>
2022-01-13 15:35:18 +0900

    singleton 예제 추가

---
+++ src/test/java/hello/singleton/SingletonService.java
@@ -0,0 +1,20 @@
+package hello.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    // 이 함수를 통해서만 SingletonService 를 사용할 수 있다.
+    public static SingletonService getInstance(){
+        return instance;
+    }
+
+    // 다른 곳에서 new 하지 못하게 private 생성자
+    private SingletonService(){
+
+    }
+
+    public void logic(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}
